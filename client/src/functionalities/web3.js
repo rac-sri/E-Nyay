7f146153c5d6080a80b87c44c8e4bed13a1f9f2a
rachit2501 <rac.sri25@gmail.com>
2020-09-01 17:23:37 +0530

    removed aave

--- client/src/functionalities/web3.js
+++ client/src/functionalities/web3.js
@@ -1,7 +1,7 @@
-import Portis from "@portis/web3"
+import Portis from "@portis/web3";
 import Web3 from "web3";
 
-const portis = new Portis("3c4b6f4b-d073-4f20-a06c-a66a9fa6214e", "ropsten");
+const portis = new Portis("3c4b6f4b-d073-4f20-a06c-a66a9fa6214e", "rinkeby");
 const web3 = new Web3(portis.provider);
 
 export default web3;
---
+++ contracts/CourtMatic.sol
@@ -0,0 +1,115 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.0;
+
+contract Court {
+    address public owner;
+    modifier onlyOwner() {
+        if (msg.sender != owner) {
+            revert();
+        }
+        _;
+    }
+    
+    struct Lawyer {
+        string name;
+        address addr;
+        string pubkey;
+        mapping(uint => string) encryptedKeys;     //for each case
+    }
+    
+    struct Judge {
+        string name;
+        address addr;
+        string pubkey;
+        mapping(uint => string) encryptedKeys;
+    }
+    
+    Lawyer[] public lawyers;
+    Judge[] public judges;
+    
+    struct Case {
+        address judge;
+        address lawyer1;
+        address lawyer2;
+        string[] evidenceFileHash;
+        string[] evidenceFileType;
+        
+        string party_1_name;
+        string party_2_name;
+        string details;
+    }
+    
+    Case[] public cases;
+    
+    event lawyerRegistered(uint _lawyerId);
+    event judgeRegistered(uint _judgeId);
+    event caseCreated(uint _caseId);
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    function registerLawyer(string memory _name, address _addr, string memory _pubkey) public {
+        Lawyer memory l = Lawyer(_name, _addr, _pubkey);
+        lawyers.push(l);
+        emit lawyerRegistered(lawyers.length-1);
+    }
+    function registerJudge(string memory _name, address _addr, string memory _pubkey) public {
+        Judge memory j = Judge(_name, _addr, _pubkey);
+        judges.push(j);
+        emit judgeRegistered(judges.length-1);
+    }
+    
+    function newCase(uint _judgeId, uint _lawyer1Id, uint _lawyer2Id, string memory _party_1_name, string memory _party_2_name, string memory _details) public  {
+        string[] memory empty;
+        Case memory tcase = Case(judges[_judgeId].addr, lawyers[_lawyer1Id].addr, lawyers[_lawyer2Id].addr, empty, empty, _party_1_name, _party_2_name, _details);
+        cases.push(tcase);
+        
+        emit caseCreated(cases.length-1);
+    }
+    
+    // Evidence
+    function uploadEvidence(uint _caseId, string memory _fileHash, string memory _fileType) public {
+        // to compare String without importing StringUtils Contract:
+        require(keccak256(bytes(_fileHash)) != keccak256(bytes("")) && keccak256(bytes(_fileType)) != keccak256(bytes("")));    //here checking string Not Null
+        cases[_caseId].evidenceFileHash.push(_fileHash);
+        cases[_caseId].evidenceFileType.push(_fileType);
+    }
+    
+    function getEvidenceCount(uint _caseId) public view returns(uint) {
+        return cases[_caseId].evidenceFileHash.length;
+    }
+    function getEvidence(uint _caseId, uint _evidenceNo) public view returns(string memory FileHash, string memory FileType) {
+        return (cases[_caseId].evidenceFileHash[_evidenceNo], cases[_caseId].evidenceFileType[_evidenceNo]);
+    }
+    
+    //EncryptedKeys
+    function addEncryptedKey(bool _isLawyer, uint _ljId, uint _caseId, string memory _key) public {
+        if(_isLawyer) {
+            lawyers[_ljId].encryptedKeys[_caseId] = _key;
+        } else {
+            judges[_ljId].encryptedKeys[_caseId] = _key;
+        }
+    }
+    function getEncryptedKey(bool _isLawyer, uint _ljId, uint _caseId) public view returns(string memory) {
+        if(_isLawyer) {
+            return lawyers[_ljId].encryptedKeys[_caseId];
+        } else {
+            return judges[_ljId].encryptedKeys[_caseId];
+        }
+    }
+    
+    function getCaseAddresses(uint _caseId) public view returns(address judge, address lawyer1, address lawyer2) {
+        return(cases[_caseId].judge, cases[_caseId].lawyer1, cases[_caseId].lawyer2);
+    }
+    
+    function getJudgesCount() external view returns(uint) {
+        return judges.length;
+    }
+    function getLawyersCount() external view returns(uint) {
+        return lawyers.length;
+    }
+    function getCasesCount() external view returns(uint) {
+        return cases.length;
+    }
+}
